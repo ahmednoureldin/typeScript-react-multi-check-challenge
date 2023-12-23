82c7dfd039fe235a3d8e89ddd655c7f528bfc366
Ahmed Nour Eldin <ahmedmedhat88@hotmail.com>
2023-12-23 14:49:16 +0200

    Checkboxes Logic

--- multicheck-challenge/src/App.tsx
+++ multicheck-challenge/src/App.tsx
@@ -1,16 +1,46 @@
 import { useState } from "react";
-import { optionsArray } from "./components/checkbox/optionsArray";
 import Checkbox from "./components/checkbox/Checkbox";
+import { optionsArray } from "./components/checkbox/optionsArray";
 import styles from "./App.module.css";
 
 const App = () => {
   const [selectAll, setSelectAll] = useState(false);
   const [options, setOptions] = useState(optionsArray);
 
+  const handleSelectAllChange = () => {
+    const newOptions = options.map((option) => ({
+      ...option,
+      checked: !selectAll,
+    }));
+    setOptions(newOptions);
+    setSelectAll(!selectAll);
+  };
+
+  const handleOptionChange = (index: number) => {
+    const newOptions = [...options];
+    newOptions[index].checked = !newOptions[index].checked;
+    setOptions(newOptions);
+
+    const allChecked = newOptions.every((option) => option.checked);
+
+    setSelectAll(allChecked);
+  };
+
   return (
-    <div className={styles.multiCheck}>
-      <h2>my-multi-check</h2>
-      <Checkbox checked={false} onChange={() => {}} label="Select All" />
+    <div className={styles.multicheckOptions}>
+      <Checkbox
+        checked={selectAll}
+        onChange={handleSelectAllChange}
+        label="Select All"
+      />
+      {options.map((option, index) => (
+        <Checkbox
+          key={index}
+          checked={option.checked}
+          onChange={() => handleOptionChange(index)}
+          label={option.label}
+        />
+      ))}
     </div>
   );
 };
