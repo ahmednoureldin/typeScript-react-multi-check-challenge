caa899ce024f697e2b32027df2cb24706113f726
Ahmed Nour Eldin <ahmedmedhat88@hotmail.com>
2023-12-23 14:29:11 +0200

    Importing and Initializing useState Hook

--- multicheck-challenge/src/App.tsx
+++ multicheck-challenge/src/App.tsx
@@ -1,8 +1,18 @@
-import "./App.css";
+import { useState } from "react";
+import { optionsArray } from "./components/checkbox/optionsArray";
 import Checkbox from "./components/checkbox/Checkbox";
+import styles from "./App.module.css";
 
-function App() {
-  return <Checkbox checked={false} onChange={() => {}} label="Select All" />;
-}
+const App = () => {
+  const [selectAll, setSelectAll] = useState(false);
+  const [options, setOptions] = useState(optionsArray);
+
+  return (
+    <div className={styles.multiCheck}>
+      <h2>my-multi-check</h2>
+      <Checkbox checked={false} onChange={() => {}} label="Select All" />
+    </div>
+  );
+};
 
 export default App;
